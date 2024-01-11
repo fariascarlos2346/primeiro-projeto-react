4e097f2b8efcb9571ceb68b51272af39b0594861
carloshenrique48 <fariascarlos2346@gmail.com>
2024-01-11 09:57:56 -0300

    alterações na logo e estilizando com CSS

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Logo03 from './assets/logo03.png'
 
 import {
    Container,
@@ -16,7 +17,7 @@ import {
 function App() {
   return (
     <Container>
-      <Image />
+      <Image alt="Logo-Desenvolvedor" src={Logo03}/>
       <H1>Sou Carlos Henrique estou migrando de carreira, varias decadas como motorista e agora entrando no mundo da programação, adquirindo experiência a três anos.</H1>
       <H2>Estou cursando Analise e Desenvolvimento de Sistemas no ultimo semestre, também estou na formação do DevClub para desenvolvedor Full Stack, ja conclui varios modolos dentre eles, HTML,CSS,Javascript,Node-js,React e neste periodo ja realizei diversos projetos.</H2>
       <P>Muitos dos meus projetos estão no https://github.com/fariascarlos2346 e meu linkedin para maiores informações https://www.linkedin.com/in/carloshenrique-web/. </P>
--- src/styles.js
+++ src/styles.js
@@ -4,24 +4,61 @@ import Background from './assets/logodev.png'
 export const Container = styled.div `
    background: url("${Background}");
    background-size: cover;
+
+   display: flex;
+   flex-direction: column;
+   align-items: center;
+   gap: 40px;
 `;
 
 export const Image = styled.img `
+   margin-top: 30px;
 `;
 
 export const ConteinerItens = styled.div `
+   background: linear-gradient(
+      157.44deg,
+      rgba(255, 255, 255, 0.6) 0.84%,
+      rgba(255, 255, 255, 0.6) 0.85%,
+      rgba(255, 255, 255, 0.15) 100%
+   );
+   border-radius: 61px 61px 0px 0px;
+
+   padding: 50px 36px;
+
+   display: flex;
+
+   flex-direction: column;
 `;
 
 export const H1 = styled.h1 `
+   font-style: normal;
+   font-weight: bold;
+   font-size: 34px;
+   line-height: 40px;
+   text-align: center;
+   color: red;
+   margin-bottom: 80px;
 `;
 
 export const H2 = styled.h2 `
+   font-style: normal;
+   font-weight: bold;
+   font-size: 34px;
+   line-height: 40px;
+   text-align: center;
+   color: green;
+   margin-bottom: 70px;
 `;
 
 export const P = styled.p `
+   
 `;
 
 export const InputLabel = styled.p `
+   letter-spacing: -0.408px;
+   font-style: normal;
+   f
 `;
 
 export const Input = styled.input `
